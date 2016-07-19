35bc0211600e8a818ddfd56149c25fc9fa93a1a2
Kudela <mkudela@ads.iu.edu>
2016-07-19 18:31:39 -0400

    first package

---
+++ R/package_hmk.R
@@ -0,0 +1,7 @@
+#' I want to read in data
+#' 
+#' I want to upload their analysis
+#' 
+#' @docType package
+#' @name hmk
+NULL
---
+++ R/some_function.R
@@ -0,0 +1,8 @@
+#' Some function
+#' 
+#' @param x,y two values to be summed
+#' @return the sum x+y
+#' @examples
+#' someFun(2,3)
+#' @export
+someFun <- function(x,y){ x + y }
